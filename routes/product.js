3a659569d556cfae51c2292865621d87e3516065
KingCharles_35 <sparklez81099@gmail.com>
2024-11-20 13:30:03 +0800

    member2 task

--- routes/product.js
+++ routes/product.js
@@ -3,19 +3,19 @@ const productController = require('../controllers/product.js');
 
 const router = express.Router();
 
-router.post('/createProduct', verify, verifyAdmin, productController.createProduct);
+router.post('/products/', verify, verifyAdmin, productController.createProduct);
 
-router.get("/all", verify, verifyAdmin, productController.getAllProducts);
+router.get("/products/all", verify, verifyAdmin, productController.getAllProducts);
 
-router.get("/", productController.getAllActive);
+router.get("/products/active", productController.getAllActive);
 
-router.get("/specific/:id", productController.getProduct);
+router.get("/products/:productId", productController.getProduct);
 
-router.patch("/:productId", verify, verifyAdmin, productController.updateProduct);
+router.patch("/products/:productId/update", verify, verifyAdmin, productController.updateProduct);
 
-router.patch("/:productId/archive", verify, verifyAdmin, productController.archiveProduct);
+router.patch("/products/:productId/archive", verify, verifyAdmin, productController.archiveProduct);
 
-router.patch("/:productId/activate", verify, verifyAdmin, productController.activateProduct);
+router.patch("/products/:productId/activate", verify, verifyAdmin, productController.activateProduct);
 
 
 module.exports = router;
