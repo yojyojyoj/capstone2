d5002a438bf0b147de55224e72a285ccef581e95
KingCharles_35 <sparklez81099@gmail.com>
2024-11-20 13:12:42 +0800

    member2 task

--- routes/product.js
+++ routes/product.js
@@ -3,8 +3,19 @@ const productController = require('../controllers/product.js');
 
 const router = express.Router();
 
+router.post('/createProduct', verify, verifyAdmin, productController.createProduct);
 
+router.get("/all", verify, verifyAdmin, productController.getAllProducts);
 
+router.get("/", productController.getAllActive);
+
+router.get("/specific/:id", productController.getProduct);
+
+router.patch("/:productId", verify, verifyAdmin, productController.updateProduct);
+
+router.patch("/:productId/archive", verify, verifyAdmin, productController.archiveProduct);
+
+router.patch("/:productId/activate", verify, verifyAdmin, productController.activateProduct);
 
 
 module.exports = router;
